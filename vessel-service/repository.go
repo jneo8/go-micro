4e0f9cd160317d7e6cd6a3e8824fd571b1479987
jneo8 <james0910238727@gmail.com>
2018-10-27 20:11:15 +0800

    vessel-service/repository FindAvailable

--- vessel-service/repository.go
+++ vessel-service/repository.go
@@ -1,13 +1,37 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"fmt"
-	"github.com/micro/go-micro"
 	pb "go-micro/vessel-service/proto/vessel"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
-func (repo *VesselRepository) Create(vessel *pb.Vessel) error {
-	return repo.collection().Insert(vessel)
+const (
+	dbName           = "shippy"
+	vesselCollection = "vessels"
+)
+
+type VesselRepository struct {
+	session *mgo.Session
+}
+
+// FindAvailable - checks a specification against a map of VesselRepository,
+// if capacity and max weight are below a vessels capacity and max weight,
+// then return that vessel.
+func (repo *VesselRepository) FindAvailable(spec *pb.Specification) (*pb.Vessel, error) {
+	var vessel *pb.Vessel
+
+	// Here we define a more complex query than our consignment-service's
+	// GetAll function. Here we're asking for a vessel who's max weight and
+	// capacity are greater than and equal to the given capacity and weight.
+	// We're also using the `One` function here as that's all we want.
+
+	err := repo.collection().Find(bson.M{
+		"capacity":  bson.M{"$gte": spec.Capacity},
+		"maxweight": bson.M{"$gte": spec.MaxWeight},
+	}).One(&vessel)
+	if err := nil {
+	    return nil, err
+	}
+	return vessel, nil
 }
