02b7b6813722be7f4b182f1879cd03f59bc8e7c8
jneo8 <james0910238727@gmail.com>
2018-10-27 19:59:40 +0800

    vessel-service/handler.go

--- vessel-service/handler.go
+++ vessel-service/handler.go
@@ -8,11 +8,30 @@ import (
 	pb "go-micro/vessel-service/proto/vessel"
 )
 
+type service struct {
+	session *mgo.Session
+}
+
 func (s *service) GetRepo() Repository {
 	return &VesselRepository{s.session.Clone()}
 }
 
-func (s *service) Create(ctx context.Context, req *pb.Vessel, res *pb.Response) error {
+func (s *service) FindAvailable(ctx context.Contextm, req *pb.Specification, res *pb.Response) error {
+	repo := s.GetRepo()
+	defer repo.Close()
+
+	// Find the next available vessel
+	vessel, err := repo.FindAvailable(req)
+	if err != nil {
+		return nil
+	}
+
+	// Set the vessel as part of the response message type
+	res.Vessel = vessel
+	return nil
+}
+
+func (s *service) Create(ctx context.Contextm, req *pb.Vessel, res *pb.Response) error {
 	repo := s.GetRepo()
 	defer repo.Close()
 	if err := repo.Create(req); err != nil {
